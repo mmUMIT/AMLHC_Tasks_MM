275aebd6dd652d659e5a9b492b7ee791ff1a2452
Melvin <87768811+vndmark@users.noreply.github.com>
2024-06-27 15:30:25 +0200

    restoring lost files

---
+++ src/R/task3.1.R
@@ -0,0 +1,6 @@
+#import
+data <- read.csv("AMLHC_Tasks_MM/food.csv", row.names = 1)
+
+#examples
+plot(iris$Sepal.Length, iris$Petal.Length)
+plot(iris$Sepal.Length, iris$Petal.Length, col = iris$Species)
